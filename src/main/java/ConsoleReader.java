8867b97c4f917bc03021ce9ec2530e8d04bd70d3
Kadochnikov Mikhail <kadochnikov.mihaylo@yandex.ru>
2022-10-01 12:25:46 +0500

    task-1
    Исправление ошибок №1
    
    Конструктор consoleReader теперь private.
    В consolereader убрали лишние коменты.
    Вынесли создание сканера на уровне класса, создав поле.
    Сделали инициализацию через конструктор в consolereader.
    Переименовали интерфейсы.
    Убрали public перед интерфейсами.
    
    Немного переработали ООП структуру проекта.
    
    Немного переработали ООП структуру проекта.
    
    Пофикшены оставшиеся недочеты, на которые было указано после 1ой проверки.
    
    Changes by mihaK
    
    Пофикшены оставшиеся недочеты V2.0
    
    add comments
    
    Пофикшены оставшиеся недочеты V3.0

---
+++ src/main/java/ConsoleReader.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ConsoleReader implements Reader {
+    private static final Response INPUT_RESPONSE = new Response("Input: ");
+    private final Scanner consoleScanner = new Scanner(System.in);
+    private final Writer writer;
+
+    public ConsoleReader(Writer writer) {
+        this.writer = writer;
+    }
+
+    @Override
+    public Request read() {
+        // Reading from console and return Request
+        writer.consoleWrite(INPUT_RESPONSE);
+        return new Request(consoleScanner.nextLine());
+    }
+}
---
+++ src/main/java/Response.java
@@ -0,0 +1,9 @@
+public class Response {
+    private final String answer;
+    Response(String answer) {
+        this.answer = answer;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+}
