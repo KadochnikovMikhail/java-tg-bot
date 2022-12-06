cc4d5639bae2369dd1b7e51f9a6aff9f45ffa058
Nikita Tkachenko <TkachLermontov@yandex.ru>
2022-12-06 15:44:00 +0500

    Simple quiz - quiz questions

--- src/main/java/ru/teamee/bots/Converter.java
+++ src/main/java/ru/teamee/bots/Converter.java
@@ -52,10 +52,14 @@ public class Converter {
             }
             case SECOND -> {
                 list = new ArrayList<>(
-                        Arrays.asList("Неправильный ответ", "Неправильный ответ", "Почти правильный ответ", "Правильный ответ"));
+                        Arrays.asList("I prefer to cook my meals at home",
+                                "I preferred cooking my meals at home",
+                                "I prefer cooking meals at my home",
+                                "I prefer cooking my meals at home"));
                 sendPoll = SendPoll.builder()
                         .chatId(response.getUserID().toString())
-                        .question("Опрос №2")
+                        .question("Put the words in the correct order to make sentences.\n" +
+                                "I / cooking / at / meals / my / home / prefer")
                         .isAnonymous(Boolean.FALSE)
                         .options(list)
                         .type("quiz")
@@ -66,10 +70,14 @@ public class Converter {
             }
             case THIRD -> {
                 list = new ArrayList<>(
-                        Arrays.asList("Неправильный ответ", "Неправильный ответ", "Почти правильный ответ", "Правильный ответ"));
+                        Arrays.asList("Josh threw a party for surprise his sister last week",
+                                "Josh threw a party for his sister last week to surprise",
+                                "Josh threw surprise party for his sister last week",
+                                "Josh threw a surprise party for his sister last week"));
                 sendPoll = SendPoll.builder()
                         .chatId(response.getUserID().toString())
-                        .question("Опрос №3")
+                        .question("Put the words in the correct order to make sentences.\n" +
+                                "Josh / a / threw / for / surprise / his / last / sister / party / week")
                         .isAnonymous(Boolean.FALSE)
                         .options(list)
                         .type("quiz")
@@ -80,10 +88,14 @@ public class Converter {
             }
             case FOURTH -> {
                 list = new ArrayList<>(
-                        Arrays.asList("Неправильный ответ", "Правильный ответ", "Почти правильный ответ", "Неправильный ответ"));
+                        Arrays.asList("You did leave the door open?",
+                                "Did you leave the door open?",
+                                "Did you left the door open?",
+                                "Did you leave the door opened?"));
                 sendPoll = SendPoll.builder()
                         .chatId(response.getUserID().toString())
-                        .question("Опрос №4")
+                        .question("Put the words in the correct order to make sentences.\n" +
+                                "you / the / door / open / leave / did?")
                         .isAnonymous(Boolean.FALSE)
                         .options(list)
                         .type("quiz")
@@ -94,10 +106,14 @@ public class Converter {
             }
             case FIFTH -> {
                 list = new ArrayList<>(
-                        Arrays.asList("Неправильный ответ", "Правильный ответ", "Почти правильный ответ", "Неправильный ответ"));
+                        Arrays.asList("You could please tell me where the nearest pharmacy is?",
+                                "Could you please tell me where the nearest pharmacy is?",
+                                "Please, could you tell me where the nearest pharmacy is?",
+                                "Could you please tell me the nearest pharmacy where is?"));
                 sendPoll = SendPoll.builder()
                         .chatId(response.getUserID().toString())
-                        .question("Опрос №5")
+                        .question("Put the words in the correct order to make sentences.\n" +
+                                "please / could / tell / you / where / me / the / pharmacy / nearest /is?")
                         .isAnonymous(Boolean.FALSE)
                         .options(list)
                         .type("quiz")
@@ -108,10 +124,17 @@ public class Converter {
             }
             case SIXTH -> {
                 list = new ArrayList<>(
-                        Arrays.asList("Неправильный ответ", "Правильный ответ", "Почти правильный ответ", "Неправильный ответ"));
+                        Arrays.asList("You are alright, you look kinda worried?",
+                                "Are you alright, you look kinda worried?",
+                                "Are you alright, look you kinda worried?",
+                                "Are you alright, you kinda look worried?"));
                 sendPoll = SendPoll.builder()
                         .chatId(response.getUserID().toString())
-                        .question("Опрос №6")
+                        .question("""
+                                Read the answers and try to come up with suitable questions for them. There is more than one option possible:
+                                - ...
+                                - Yes, I’m fine, just a little bit under the weather.
+                                """)
                         .isAnonymous(Boolean.FALSE)
                         .options(list)
                         .type("quiz")
@@ -122,10 +145,17 @@ public class Converter {
             }
             case SEVENTH -> {
                 list = new ArrayList<>(
-                        Arrays.asList("Неправильный ответ", "Правильный ответ", "Почти правильный ответ", "Неправильный ответ"));
+                        Arrays.asList("Didn't come up to meeting a week ago?",
+                                "Why you didn't come up to meeting a week ago?",
+                                "Who you didn't come up to meeting a week ago?",
+                                "Why you didn't came up to meeting a week ago?"));
                 sendPoll = SendPoll.builder()
                         .chatId(response.getUserID().toString())
-                        .question("Опрос №7")
+                        .question("""
+                                Read the answers and try to come up with suitable questions for them. There is more than one option possible.
+                                - ...
+                                - I was grounded because my parents learnt I got a “C” for my Calculus final.
+                                """)
                         .isAnonymous(Boolean.FALSE)
                         .options(list)
                         .type("quiz")
@@ -136,10 +166,17 @@ public class Converter {
             }
             case EIGHT -> {
                 list = new ArrayList<>(
-                        Arrays.asList("Неправильный ответ", "Правильный ответ", "Почти правильный ответ", "Неправильный ответ"));
+                        Arrays.asList("Do you think, which one fits me better blue or red dress?",
+                                "What do you think, which one fits me better blue or red dress?",
+                                "What do you think, which one fits me better blue or red?",
+                                "Which one fits me better blue or red dress?"));
                 sendPoll = SendPoll.builder()
                         .chatId(response.getUserID().toString())
-                        .question("Опрос №8")
+                        .question("""
+                                Read the answers and try to come up with suitable questions for them. There is more than one option possible.
+                                - ...
+                                - I would go with the red dress. It suits you better than the blue one.
+                                """)
                         .isAnonymous(Boolean.FALSE)
                         .options(list)
                         .type("quiz")
@@ -150,14 +187,21 @@ public class Converter {
             }
             case NINTH -> {
                 list = new ArrayList<>(
-                        Arrays.asList("Неправильный ответ", "Правильный ответ", "Почти правильный ответ", "Неправильный ответ"));
+                        Arrays.asList("Who was the 16th president of United States of America?",
+                                "Am I a president?",
+                                "Who is the 16th president of United States of America?",
+                                "Where was the 16th president of United States of America?"));
                 sendPoll = SendPoll.builder()
                         .chatId(response.getUserID().toString())
-                        .question("Опрос №9")
+                        .question("""
+                                Read the answers and try to come up with suitable questions for them. There is more than one option possible.
+                                - ...
+                                - I think it was Abraham Lincoln who abolished slavery in the USA.
+                                """)
                         .isAnonymous(Boolean.FALSE)
                         .options(list)
                         .type("quiz")
-                        .correctOptionId(1)
+                        .correctOptionId(0)
                         .explanation("тут пояснения если что-то не так сделано")
                         .protectContent(true)
                         .build();
